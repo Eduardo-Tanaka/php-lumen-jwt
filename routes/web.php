bdd7338f8a010e094c9a0b5c2acb409cd0a15713
Eduardo <eduardo.s.tanaka@gmail.com>
2022-08-04 13:06:51 -0300

    jwt initial setup

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+Route::group(['prefix' => 'api'], function ($router) {
+    Route::post('login', 'Auth\AuthController@login');
+    Route::post('logout', 'Auth\AuthController@logout');
+
+    // ROUTES WITH AUTH
+    Route::group(['middleware' => 'auth:api'], function () {
+        Route::get('me', 'UserController@me');
+        Route::get('users', 'UserController@getUsers');
+        Route::post('refresh', 'Auth\AuthController@refresh');
+    });
+});
